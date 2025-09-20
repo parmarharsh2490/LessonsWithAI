c1c2ff4d3e3a17fb9ab4ddc3d14a24fa4ae05309
Harsh <parmarharsh6480@gmail.com>
2025-09-20 17:46:27 +0530

    update auth guard.

--- src/app/modules/auth/auth-guard.ts
+++ src/app/modules/auth/auth-guard.ts
@@ -3,6 +3,7 @@ import { CanActivateFn } from '@angular/router';
 import { map, catchError, of } from 'rxjs';
 import { UserProfileService } from '../user/service/user-profile.service';
 import { CommonService } from '../../services/common-service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export const authGuard: CanActivateFn = () => {
   const commonService = inject(CommonService);
@@ -13,9 +14,13 @@ export const authGuard: CanActivateFn = () => {
     map(() => {
       return true;
     }),
-    catchError(() => {
-      window.location.href =
-        'http://localhost:8081/oauth2/authorization/keycloak';
+    catchError((error) => {
+      if (error instanceof HttpErrorResponse) {
+        if (error.status === 403) {
+          window.location.href =
+            'http://localhost:8081/oauth2/authorization/keycloak';
+        }
+      }
       return of(false);
     }),
   );
