7d0336ce8be7411e87bbf67164fcb74667f7cba1
Parmar Harsh <parmarharsh6480@gmail.com>
2025-11-16 19:09:46 +0530

    Update environment URLs for production

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  baseUrl: 'http://localhost:8081/api/v1/',
-  keyCloakUrl: 'http://localhost:8080',
+  baseUrl: 'https://lessonswithai-backend-87c0.onrender.com/api/v1/',
+  keyCloakUrl: 'https://keycloak-nightly-kdd6.onrender.com',
   keyCloakRealm: 'lessonswithai',
   keyCloakClientId: 'lessonswithai',
-  vapiPublicKey: '5864f127-79a4-4e01-a970-9b1c74c05de8',
+  vapiPublicKey: 'd26495e9-7ced-49ae-8914-cd0ec8d04fd2',
 };
