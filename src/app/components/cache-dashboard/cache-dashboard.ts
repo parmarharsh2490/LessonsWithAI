6fca63d007a87fd67e9acf1113adc322e7bc4157
Harsh <parmarharsh6480@gmail.com>
2025-11-24 08:14:58 +0530

    show cache dashboard only when it is set on localstorage and add custom window event to enable.

--- src/app/components/cache-dashboard/cache-dashboard.ts
+++ src/app/components/cache-dashboard/cache-dashboard.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CachingService } from '../../core/services/caching.service';
 import { Button } from 'primeng/button';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { CommonService } from '../../services/common-service';
 
 @Component({
   selector: 'app-cache-dashboard',
@@ -10,12 +11,23 @@ import { TableModule } from 'primeng/table';
   templateUrl: './cache-dashboard.html',
   styleUrl: './cache-dashboard.scss',
 })
-export class CacheDashboard {
+export class CacheDashboard implements OnInit {
   cacheService = inject(CachingService);
+  commonService = inject(CommonService);
 
   isExpanded = signal(false);
+  showCacheDashboard = signal(false);
 
   expand() {
     this.isExpanded.set(!this.isExpanded());
+  }
+
+  ngOnInit(): void {
+    if (!this.commonService.isBrowser) return;
+    window.addEventListener('showCacheDashboard', () => {
+      this.showCacheDashboard.set(
+        localStorage.getItem('showCacheDashboard') === 'true',
+      );
+    });
   }
 }
